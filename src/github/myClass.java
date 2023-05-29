365ea77423898a51e6541b382817ee03becabecb
Asmar93 <mhammad.barghothi@gmail.com>
2023-05-29 10:39:22 +0300

    update

---
+++ src/github/myClass.java
@@ -0,0 +1,12 @@
+package github;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String myName = "Asmar";
+		int myAge = 30;
+	}
+
+}
